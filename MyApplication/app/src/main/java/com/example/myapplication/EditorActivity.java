e0eaa984c4617ef1c465781e279dd15ec3f841d4
Paweł Wojtaszko <wojtaszko@objectconnect.de>
2019-01-30 15:15:18 +0100

    Added editor library and simple editing activity

---
+++ MyApplication/app/src/main/java/com/example/myapplication/EditorActivity.java
@@ -0,0 +1,48 @@
+package com.example.myapplication;
+
+import android.graphics.Typeface;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ja.burhanrashid52.photoeditor.PhotoEditor;
+import ja.burhanrashid52.photoeditor.PhotoEditorView;
+
+public class EditorActivity extends AppCompatActivity {
+
+    PhotoEditor mPhotoEditor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+
+        PhotoEditorView mPhotoEditorView = findViewById(R.id.photoEditorView);
+
+        String type = getIntent().getStringExtra("type");
+        String color = getIntent().getStringExtra("color");
+
+
+        if(type.equals("1")){
+            switch (color){
+                case "white":
+                    mPhotoEditorView.getSource().setImageResource(R.drawable.tshirt1_white);
+                    break;
+            }
+        }
+
+        //Use custom font using latest support library
+        //Typeface mTextRobotoTf = ResourcesCompat.getFont(this, R.font.roboto_medium);
+
+        //loading font from assest
+        //Typeface mEmojiTypeFace = Typeface.createFromAsset(getAssets(), "emojione-android.ttf");
+
+        mPhotoEditor = new PhotoEditor.Builder(this, mPhotoEditorView)
+                .setPinchTextScalable(true)
+//                .setDefaultTextTypeface(mTextRobotoTf)
+//                .setDefaultEmojiTypeface(mEmojiTypeFace)
+                .build();
+
+        mPhotoEditor.setBrushDrawingMode(true);
+    }
+}
---
+++ MyApplication/app/src/main/java/com/example/myapplication/TshirtTypeChooser.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+
+public class TshirtTypeChooser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tshirt_type_chooser);
+
+        CardView tshirt1 = findViewById(R.id.tshirt1);
+        tshirt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), EditImageActivity.class);
+                intent.putExtra("type", "1");
+                intent.putExtra("color", "white");
+
+                startActivity(intent);
+            }
+        });
+    }
+}
